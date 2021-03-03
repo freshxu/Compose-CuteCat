a51442dafd19b10281b42f2c2a6c2c8bc24b87d1
xushuyang <xushuyang@xiaomi.com>
2021-03-03 16:51:48 +0800

    fix build error

--- app/src/main/java/com/example/androiddevchallenge/CatList.kt
+++ app/src/main/java/com/example/androiddevchallenge/CatList.kt
@@ -16,8 +16,8 @@
 package com.example.androiddevchallenge
 
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.runtime.Composable
 import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androiddevchallenge.model.Cat
